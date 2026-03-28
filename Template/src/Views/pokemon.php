0adc901beddce0a724f95e108325488afcb96041
Chiara <ctolosano@edenschool.fr>
2026-03-28 18:31:04 +0100

    Update pokemon.php

--- Template/src/Views/pokemon.php
+++ Template/src/Views/pokemon.php
@@ -1,6 +1,7 @@
 <?php foreach ($pokemons as $pokemon): ?>
     <div class="card">
         <h2><?= $pokemon->getNom(); ?></h2>
+        <p><?= $pokemon->getDescription(); ?></p>
         <p>Type : <?= $pokemon->getType(); ?></p>
         <img src="<?= $pokemon->getImageUrl(); ?>" alt="">
     </div>
